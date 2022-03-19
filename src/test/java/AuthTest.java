d0cfca52a6db62d4d9bab954e5942fd477e5985f
arishkinnov777 <arina-novikova2009@yandex.ru>
2022-03-19 10:29:37 +0300

    add test

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,25 +1,33 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AuthTest {
 
     @Test
     public void shouldAuthorizeTest() {
 
-        open("https://github.com/");
-        $("[href = '/login']")
+        open("https://github.com/"); //Открыть форму авторизации
+
+        $("[href = '/login']") //Залогинтся в гитхабе
                 .click();
         $("[id='login_field']")
                 .sendKeys("arina-novikova2009@yandex.ru");
         $("[id='password']")
                 .sendKeys("2403Ndaas2003");
-        //4. Нажать кнопку sign in
         $(".js-sign-in-button")
                 .click();
         $(".Header")
                 .shouldBe(visible);
+
+        $(".js-feature-preview-indicator-container") //переход в личный профиль
+                .click();
+        $(byText("Your profile"))
+                .click();
+        $(byText("Arina Novikova"))
+                .shouldHave(visible);
+
     }
 }
