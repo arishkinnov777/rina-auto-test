052c72a86870f687904d3a8d2735164f16e813a3
arishkinnov777 <arina-novikova2009@yandex.ru>
2022-06-01 14:05:55 +0300

    убрала анатацию Test

--- src/test/java/GithubTest.java
+++ src/test/java/GithubTest.java
@@ -4,13 +4,19 @@ import io.qameta.allure.Owner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.Keys;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @Owner("arishkinnov")
 @Feature("Ветки гита")
@@ -38,9 +44,13 @@ public class GithubTest {
         });
     }
 
-    @Test
-    @DisplayName("Выбор ветки")
-    public void shouldSearchByReleaseTest() {
+    @MethodSource("findBy")
+    @ParameterizedTest(name = "{0}")
+    @DisplayName("Поиск по ")
+    public void shouldSearchByReleaseTest(String type,
+                                          String value,
+                                          Integer size) {
+
         step("Перейти на страницу 'Releases'", () -> {
             TestPage.githubPage.releasesLink()
                     .click();
@@ -48,24 +58,35 @@ public class GithubTest {
                     .shouldBe(visible);
         });
 
-        step("Проверить, сколько записей найдется по значению в буквах", () -> {
+        step("Проверить, сколько записей найдется по значению ", () -> {
+
             TestPage.githubPage.findInput() //ввести значение
-                    .sendKeys("RC");
+                    .sendKeys(value);
             TestPage.githubPage.findInput(). //нажать на кнопку ENTER
                     sendKeys(Keys.ENTER);
-            TestPage.githubPage.releasesList(). //проверить сколько строк вышло по значению RC
-                    shouldHave(size(2));
+            TestPage.githubPage.releasesList(). //проверить сколько строк вышло по значению
+                    shouldHave(size(size));
         });
+    }
 
-        step("Проверить, сколько записей найдется по значению в цифрах", () -> {
-            TestPage.githubPage.findInput()// очистить инпут
-                    .clear();
-            TestPage.githubPage.findInput()
-                    .sendKeys("4.13");//ввести значение
-            TestPage.githubPage.findInput() //нажать на кнопку ENTER
-                    .sendKeys(Keys.ENTER);
-            TestPage.githubPage.releasesList().//проверить сколько строк вышло по значению 4.13
-                    shouldHave(size(6));
-        });
+    static Stream<Arguments> findBy() {
+        return Stream.of(
+                arguments(
+                        "фрагмету состоящем из цифр",
+                        "4.13",
+                        "6"
+
+                ),
+                arguments(
+                        "фрагмету состоящем из букв",
+                        "RC",
+                        "2"
+                ),
+                arguments(
+                        "полному названию",
+                        "JUnit 4.13 RC 2",
+                        "1"
+                )
+        );
     }
 }
